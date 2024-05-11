c4bd975bc9f93680a58717b11706fe10387c3c29
01604987 <e1604987@student.tuwien.ac.at>
2024-05-11 14:12:58 +0200

    rm obsolete code

--- main/main.c
+++ main/main.c
@@ -108,39 +108,6 @@ static void printDirectory(char * path) {
 	closedir(dir);
 }
 
-// float magnitude(float x, float y, float z) {
-//     return sqrt(x*x + y*y + z*z);
-// }
-
-// int count_steps(float mag_array[], int size, float threshold) {
-//     int step_count = 0;
-//     bool above_threshold = false; // Flag to track if the magnitude is above the threshold
-
-//     for (int i = 1; i < size - 1; i++) {
-//         if (mag_array[i] > threshold) {
-
-// 			if (mag_array[i-1] < mag_array[i]){
-// 				// If magnitude is above the threshold
-// 				if (!above_threshold) {
-// 					// If not already above threshold, increment step count
-// 					step_count++;
-// 					above_threshold = true; // Set the flag
-// 				}
-// 			}
-            
-//         } else {
-//             above_threshold = false; // Reset the flag if magnitude falls below threshold
-//         }
-//     }
-
-//     return step_count;
-// }
-
-// float apply_filter(float prev_output, float curr_input, float prev_input, float* a, float* b){
-// 	float output = a[0] * prev_output + b[0] * curr_input + b[1] * prev_input;
-// 	return output;
-// }
-
 void end_callback(TimerHandle_t xTimer) {
     printf("Timer reached, sending end cmd");
 	end = 1;
@@ -182,7 +149,7 @@ void app_main(void)
 		uint8_t output_index = 0;
 
 				
-		// TODO put these into postproc
+		// TODO put these 4 following vars into postproc
 		char* walking = "WALKING";
 		char* idle = "IDLE";
 
@@ -261,125 +228,3 @@ void app_main(void)
 		}
 	}
 }
-
-
-	// while (1) {
-	// 	init_network();
-	// 	if (connect_to_sock() != 1){
-	// 		break;
-	// 	};
-
-	// 	float accBuf[3];
-
-
-	// 	uint8_t Fs = 100;
-	// 	uint8_t interval = 50;
-	// 	float mag_output[interval];
-	// 	float mag_input[2];
-
-	// 	float a[] = {0.8277396};
-	// 	float b[] = {0.0861302, 0.0861302};
-		
-	// 	// based on fft hz
-	// 	float threshold = 0.30;
-
-
-	// 	float running_sum = 0;
-	// 	float mean = 0;
-
-	// 	int mag_output_counter = 0; 
-	// 	int mag_input_counter = 0;
-	// 	int step_counter = 0;
-	// 	int interval_steps = 0;
-	// 	int feature = 0;
-
-	// 	int mean_counter = 1;
-		
-
-	// 	char* walking = "WALKING";
-	// 	char* idle = "IDLE";
-		
-	// 	extern int conn_err;
-
-	// 	if (xTimerStart(end_timer, 0) != pdPASS) {
-    //     // Timer start failed
-    //     // Handle the error
-    //     }
-	// 	while (1) {
-
-	// 		// TODO use timer instead of sleep, frequency will change due to accumulated processing time + sleep
-	// 		if (mag_output_counter >= WINDOW){
-	// 			interval_steps = count_steps(mag_output, interval, threshold);
-	// 			if (interval_steps > 0){
-	// 				step_counter += interval_steps;
-	// 				feature = 1;
-	// 				clear_screen();
-	// 				draw_steps(step_counter);
-	// 				draw_text(walking);
-	// 			} else {
-	// 				if (feature == 1){
-	// 					feature = 0;
-	// 					clear_screen();
-	// 					draw_steps(step_counter);
-	// 					draw_text(idle);
-	// 				}
-	// 			}
-	// 			mag_output_counter = 0;
-	// 			continue;
-	// 		}
-
-
-	// 		vTaskDelay(10 / portTICK_PERIOD_MS);
-	// 		getAccelData(&accBuf[0], &accBuf[1], &accBuf[2]);
-	// 		// calculate magnitude
-	// 		float mag = magnitude(accBuf[0], accBuf[1], accBuf[2]);
-	// 		running_sum += mag;
-	// 		// calculate running mean for normalization
-	// 		mean = (running_sum) / mean_counter;
-
-	// 		// calculate normalized input magnitude
-	// 		mag_input[mag_input_counter] = mag - mean;
-
-	// 		// output array must be already length = 1 in order to apply current filter
-	// 		if (mag_output_counter < 1){
-	// 			mag_output[mag_output_counter] = mag_input[mag_input_counter];
-	// 		} else {
-	// 			// pass in previous input, current input, previous input and both coefficients
-	// 			mag_output[mag_output_counter] = apply_filter(mag_output[mag_output_counter - 1], mag_input[mag_input_counter], mag_input[mag_input_counter ^ 1], a, b);
-	// 		}
-	// 		//clear_screen();
-	// 		//draw_mags(mag_input[mag_input_counter], mag_output[mag_output_counter]);
-			
-	// 		//draw_mag(mean);
-
-	// 		mag_output_counter += 1;
-	// 		mag_input_counter = mag_input_counter ^ 1;
-	// 		mean_counter += 1;
-
-	// 		if (conn_err == 1){
-	// 			break;
-
-	// 		} else {
-	// 			if (end == 1){
-	// 				const char *message = "end";
-	// 				send_buf(message, sizeof(message)-1);
-
-	// 				const char *response = recv_buf();
-	// 				if (strcmp(response, "Bye!")) {
-	// 					break;
-	// 				}	
-
-	// 			} else {
-	// 				send_buf(accBuf, sizeof(accBuf));
-	// 			}
-	// 		}
-	// 	}
-
-	// 	if(check_conn() == 1){
-	// 		ESP_LOGE(TAG, "Shutting down socket and restarting...");
-    //         shutdown_conn();
-    //         close_sock();
-	// 	}
-	// }
-
-//}
